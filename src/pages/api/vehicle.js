78557ea0b03b0a074368e9ba3fde7a8436d593ca
felipemuner <felipe.muner@gmail.com>
2020-07-04 10:04:49 +0700

    set up typescript

---
+++ src/pages/api/vehicle.js
@@ -0,0 +1,38 @@
+// Fake users data
+const vehicles = [
+  {
+    "vehicle": "car",
+    "ownerName": "Felipe",
+    "details": "I30",
+  },
+  {
+    "vehicle": "car4",
+    "ownerName": "Felipe",
+    "details": "I30",
+  },
+  {
+    "vehicle": "car2",
+    "ownerName": "Felipe2",
+    "details": "I302",
+  },
+  {
+    "vehicle": "car3",
+    "ownerName": "Felipe3",
+    "details": "I303",
+  },
+];
+
+export default function handler(req, res) {
+  console.log(req.query)
+  let filteredVehicles = vehicles
+  if(req.query.ownerName){
+    filteredVehicles = vehicles.filter(v => req.query.ownerName === v.ownerName)
+  }
+
+  if(req.query.ownerName){
+    filteredVehicles = vehicles.filter(v => req.query.vehicle === v.vehicle)
+  }
+
+  // Get data from your database
+  res.status(200).json(filteredVehicles);
+}
