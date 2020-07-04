22a1f903b20c16dadd127711c398ba9dcba101ee
felipemuner <felipe.muner@gmail.com>
2020-07-04 11:51:03 +0700

    refactor to use typescript from now on

--- src/pages/api/vehicle.js
+++ src/pages/api/vehicle.js
@@ -35,4 +35,5 @@ export default function handler(req, res) {
 
   // Get data from your database
   res.status(200).json(filteredVehicles);
+  
 }
--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,21 +1,16 @@
 import Link from 'next/link'
 import {useEffect, useState} from 'react';
+import { VehiclePerson } from './api/VehiclePerson';
 
-export default function List({ownersList}){
-  // console.log(ownersList)
-  // const [owners, setOwners] = useState([])
-  // useEffect(() =>{
-  //   async function loadData(){
-  //     const response = await fetch('/api/vehicle')
-  //     const ownersList = await response.json();
-  //     setOwners(ownersList)
-  //   }
-  //   loadData();
-  // }, [])
+export interface ListProps {
+  ownersList: VehiclePerson[] | undefined
+}
 
+export default function List({ownersList} : ListProps){
   return (
     <div>
-      {ownersList.map((e,index) =>(
+
+      {ownersList?.map((e,index) =>(
         <div key={index}>
           <Link as={`/${e.vehicle}/${e.ownerName}`} href="/[vehicle]/[person]">
             <a>
@@ -30,6 +25,6 @@ export default function List({ownersList}){
 
 List.getInitialProps = async () => {
   const response = await fetch('http://localhost:3000/api/vehicle')
-  const ownersList = await response.json();
+  const ownersList: VehiclePerson[] | undefined = await response.json();
   return {ownersList: ownersList}
 }
